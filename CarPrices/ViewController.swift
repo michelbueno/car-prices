367c3f96e8d6fdd5e8261d674b46b6c1a8acdb22
michelbueno <bueno.michel@gmail.com>
2015-07-23 17:53:08 -0300

    Initial Commit

---
+++ CarPrices/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarPrices
+//
+//  Created by mbueno on 7/23/15.
+//  Copyright (c) 2015 Michel Bueno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
